0e7cd9e95d0783188394a69ffb19e11f7e8192f9
conradbuck <buckconrad0@gmail.com>
2024-11-16 18:53:43 -0800

    added rotary good algo

--- winston-pomodoro-timer-rev2/firmware/firmware.ino
+++ winston-pomodoro-timer-rev2/firmware/firmware.ino
@@ -19,3 +19,34 @@ void loop() {
 
 	}
 }
+
+/*
+Rotary encoder algo
+uint8_t lrmem = 3;
+int lrsum = 0;
+int rotary()
+{
+  static int8_t TRANS[] = {0, -1, 1, 14, 1, 0, 14, -1, -1, 14, 0, 1, 14, 1, -1, 0};
+  int8_t l, r;
+
+  l = digitalRead(LEFT);
+  r = digitalRead(RIGHT);
+  lrmem = ((lrmem & 0x03) << 2) + 2 * l + r;
+  lrsum = lrsum + TRANS[lrmem];
+  /* encoder not in the neutral state */
+  if (lrsum % 4 != 0) return (0);
+  /* encoder in the neutral state */
+
+  if (lrsum == 4) {
+    lrsum = 0;
+    return (1);
+  }
+  if (lrsum == -4) {
+    lrsum = 0;
+    return (-1);
+  }
+  /* lrsum > 0 if the impossible transition */
+  lrsum = 0;
+  return (0);
+}
+ */
